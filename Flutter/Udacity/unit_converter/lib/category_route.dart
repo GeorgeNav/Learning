93b7dd98a5ff9a4ea4afe0113600a88cbb0c368f
GeorgeNav <george.g.navarro@gmail.com>
2018-12-18 13:19:43 -0600

    Flutter: making progress #Udacity

--- Flutter/Udacity/unit_converter/lib/category_route.dart
+++ Flutter/Udacity/unit_converter/lib/category_route.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'category.dart';
 
 // TODO: Check if we need to import anything
 
@@ -40,19 +41,40 @@ class CategoryRoute extends StatelessWidget {
     Colors.red,
   ];
 
+  Widget listViewCreator(List<Category> categories) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int i) => categories[i],
+      itemCount: categories.length,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: Create a list of the eight Categories, using the names and colors
     // from above. Use a placeholder icon, such as `Icons.cake` for each
     // Category. We'll add custom icons later.
+    final categories = <Category>[]; // List of categories
+    for(var i = 0; i < _categoryNames.length; i++)
+      categories.add(
+        Category(
+          name: _categoryNames[i],
+          color: _baseColors[i],
+          iconLocation: Icons.cake,
+      ));
 
     // TODO: Create a list view of the Categories
-    final listView = Container();
+
+    final listView = Container(
+      color: Colors.transparent,
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      child: listViewCreator(categories),
+    );
 
     // TODO: Create an App Bar
-    final appBar = AppBar();
-
-    return Scaffold(
+    final appBar = AppBar(
+      title: Text('Unit Converter App'),
+    );
+    return Scaffold( // displays AppBar
       appBar: appBar,
       body: listView,
     );
