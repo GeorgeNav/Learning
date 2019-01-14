2f2770309a912d377a6a59cc5b71096b4e62a6bf
GeorgeNav <george.g.navarro@gmail.com>
2019-01-14 12:00:25 -0600

    mobile test

--- Github/Udacity/git.sh
+++ Github/Udacity/git.sh
@@ -2,6 +2,7 @@
 # + means line was added
 # - means line was removed
     diff -u file1.extension file2.extension
+    diff --staged; # compares staged files to latest commit?
 # Add changes to future commit
     git add -A;
     git add .;
@@ -17,4 +18,9 @@
     git pull origin master;
     git pull origin master --allow-unrelated-histories;
 # Push the committed changes
-    git push;
+    git push -u origin master # sets this remote (origin) and branch (master) as default when pushing
+    git push; # after default is set, this will push changes to default remote and branch
+# Revert changes to a file to a previous commit
+    git log; git log --staged; # git history of commits, copy SHA key
+    git revert <SHA key>; # revert changes of a specified commit
+    git revert HEAD; # revert changes of the lastest commit
